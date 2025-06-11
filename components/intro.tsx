150c55b183eb14f6758ba83efa132195500f2d43
Felipe Gonzalez <contraviento@gmail.com>
2025-06-10 18:22:17 -0700

    Added a wrapper div with minHeight for the TypeAnimation to ensure consistent layout and eliminate initial render glitch

--- components/intro.tsx
+++ components/intro.tsx
@@ -19,22 +19,38 @@ const Intro = () => {
    }, [])
 
    return (
-      <section className='grid grid-cols-1 items-center justify-center sm:grid-cols-1 md:grid-cols-1'>
+      <section className='fade-in grid grid-cols-1 items-center justify-center sm:grid-cols-1 md:grid-cols-1'>
          {/* Content Section */}
          <div className='col-span-1 mx-auto flex flex-col items-start gap-1'>
-            <h1 className='title lh- scroll-mt-10 leading-10 font-extrabold text-pretty no-underline' ref={titleRef}>
+            <h1
+               className='title lh- scroll-mt-10 leading-10 font-extrabold text-pretty no-underline'
+               ref={titleRef}
+            >
                Hey there, I&#39;m Felipe
             </h1>
-            <TypeAnimation
-               sequence={['Front-end Developer', 400, 'Web Designer', 400, 'Enthusiast', 400]}
-               speed={40}
-               repeat={Infinity}
-               className='font-serif text-xl text-[var(--text-color)] sm:text-2xl'
-            />
+            <div style={{ minHeight: '2.5em' }}>
+               <TypeAnimation
+                  sequence={[
+                     'Full-Stack Developer',
+                     400,
+                     'Front-End Developer',
+                     400,
+                     'Web Designer',
+                     400,
+                     'Enthusiast',
+                     400
+                  ]}
+                  speed={40}
+                  repeat={Infinity}
+                  className='font-serif text-xl text-[var(--text-color)] sm:text-2xl'
+               />
+            </div>
             <p className='text text-md mt-3 font-light text-pretty sm:text-lg'>
-               With over 4 years of experience as a<span className='font-bold'> Front-End Developer</span> and additional years as a Web Designer, I am skilled in{' '}
-               <span className='font-bold'>HTML, CSS, JavaScript</span> and <span className='font-bold'>React.</span> I have a strong focus on writing clean, maintainable code and performance to build
-               efficient, user-friendly web applications.
+               With over 4 years of experience as a
+               <span className='font-bold'> Front-End Developer</span> and additional years as a Web
+               Designer, I am skilled in <span className='font-bold'>HTML, CSS, JavaScript</span>{' '}
+               and <span className='font-bold'>React.</span> I have a strong focus on writing clean,
+               maintainable code and performance to build efficient, user-friendly web applications.
             </p>
          </div>
       </section>
