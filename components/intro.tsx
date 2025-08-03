e3bd63c8d7cfbb2eeef3e27471bbe001847303cb
Felipe Gonzalez <contraviento@gmail.com>
2025-08-02 22:18:20 -0700

    Added aria-live polite to the type animation block for better screen reader suppor

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,17 @@
 import { useEffect, useRef } from 'react'
 import { TypeAnimation } from 'react-type-animation'
 
+const animationSequence = [
+   'Full-Stack Developer',
+   400,
+   'Front-End Developer',
+   400,
+   'Web Designer',
+   400,
+   'Enthusiast',
+   400
+]
+
 const Intro = () => {
    const titleRef = useRef<HTMLHeadingElement>(null)
 
@@ -16,26 +27,17 @@ const Intro = () => {
    }, [])
 
    return (
-      <section className='fade-in grid grid-cols-1 items-center justify-center sm:grid-cols-1 md:grid-cols-1'>
-         <div className='col-span-1 mx-auto flex flex-col items-start gap-1'>
+      <section className='fade-in flex items-center justify-center'>
+         <div className='mx-auto flex flex-col items-start gap-1'>
             <h1
                className='title lh- scroll-mt-10 leading-10 font-extrabold text-pretty no-underline'
                ref={titleRef}
             >
                Hey there, I&#39;m Felipe
             </h1>
-            <div className='type-animation-block'>
+            <div className='type-animation-block' aria-live='polite'>
                <TypeAnimation
-                  sequence={[
-                     'Full-Stack Developer',
-                     400,
-                     'Front-End Developer',
-                     400,
-                     'Web Designer',
-                     400,
-                     'Enthusiast',
-                     400
-                  ]}
+                  sequence={animationSequence}
                   speed={40}
                   repeat={Infinity}
                   className='font-serif text-xl text-[var(--text-color)] sm:text-2xl'
